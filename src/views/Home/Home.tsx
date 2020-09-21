18861838804ef73a0f6d5424b261b16daac666a8
bucketYang <515971947@qq.com>
2020-09-21 16:36:02 +0800

    update countdown

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -8,6 +8,7 @@ import PageHeader from '../../components/PageHeader'
 import Spacer from '../../components/Spacer'
 import Balances from './components/Balances'
 
+import Countdown from 'react-countdown';
 const Home: React.FC = () => {
   return (
     <Page>
@@ -16,15 +17,23 @@ const Home: React.FC = () => {
         title="DARKSWAP"
         subtitle=""
       />
-
+      <Styledctd>
+        <b>Launch:</b> <br /><Countdown date={Date.UTC(2020,8,22,9,0,0)}/> <br />
+      </Styledctd>
+      
       <Container>
         <Balances />
       </Container>
       <Spacer size="lg" />
       <StyledInfo>
-        <b>Hidden Tip</b>: DARK-ETH/RI/yUSD UNI-V2 pools yields 10 times more
+        <b>Dark Tip</b>: DARK-ETH/RI/yUSD UNI-V2 pools yields 10 times more
+        rewards per block. <br />
+      </StyledInfo>
+      <StyledInfo>
+        <b>Bright Tip</b>: XIOT-ETH/BXIOT/RI UNI-V2 pools yields 6 times more
         rewards per block.
       </StyledInfo>
+
       <Spacer size="lg" />
       <div
         style={{
@@ -41,7 +50,20 @@ const StyledInfo = styled.h3`
   color: ${(props) => props.theme.color.grey[500]};
   font-size: 16px;
   font-weight: 400;
-  margin: 0;
+  margin: 10px 0;
+  padding: 0;
+  text-align: center;
+
+  > b {
+    color: ${(props) => props.theme.color.grey[600]};
+  }
+`
+
+const Styledctd = styled.h3`
+  color: ${(props) => props.theme.color.grey[100]};
+  font-size: 24px;
+  font-weight: 700;
+  margin: 0 0 20px 0;
   padding: 0;
   text-align: center;
 
